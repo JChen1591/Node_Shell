7623026109add3e8a4a5d2c2504ff212e901599e
Jenny Chen <chen.jenny15@yahoo.com>
2022-09-21 16:10:02 -0400

    working on pwd

---
+++ bash.js
@@ -0,0 +1,8 @@
+//Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on(pwd => {
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+});
